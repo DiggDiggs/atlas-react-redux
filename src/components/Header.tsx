ae9b4c7208141417c19d6a3471c3284cba7660e0
Christopher Diggs <65854363+DiggDiggs@users.noreply.github.com>
2024-09-26 01:39:29 +0000

    documated the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
 import logo from "../../mockup/logo.png";
 
-
+/**
+ * Header Component
+ *
+ * This component renders a sticky header with a logo image at the top of the page.
+ */
 export const Header = () => {
+  console.log("Rendering Header"); // Log when the Header component is rendered
+
   return (
     <header className="sticky top-0 border-b-2 border-blue bg-off-white-light pb-8 pt-8">
       <img className="logo mx-auto w-56" src={logo} alt="logo" />
